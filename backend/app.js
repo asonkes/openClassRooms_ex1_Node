ddba08f8de4e1d40aac6f9dad9003ee173081f7b
asonkes <audrey.sonkes@gmail.com>
2025-12-22 13:07:46 +0100

    route 'get' et route 'post' ok

--- backend/app.js
+++ backend/app.js
@@ -9,33 +9,56 @@ const express = require('express');
 // Mais qui permet de créer une application express
 const app = express();
 
+// ca va intercepter toutes les requetes qui ont un content-type Json
+// Et rajouter les données dans le corps de l'application
+app.use(express.json());
+
+// On met le 'use' ici, car cela vaut pour le 'get' et le 'post'
 app.use((req, res, next) => {
-    console.log('Requête reçue ! ');
-    // Si on ne met pas ça, on va voir dans le terminal : requête reçue
-    // Mais ça va pas passer à la suivante
+    // Là, avec '*', on dit que l'origin qui a le droit d'accéder à notre API
+    // C'est tout le monde === *
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // On donne le droit d'utiliser certains 'header'
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // On donne le droit d'utiliser certaines méthodes
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 })
 
-// Ici on va donc créer encore un middleware
-// Pour essayer de modifier le code de la réponse 'http'
-app.use((req, res, next) => {
-    res.status(201);
-    next();
+// Et on fait un middelware pour 'POST'
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    // On est obligé d'envoyer une réponse, ausinon la requête va planter
+    // Donc va faire semblant qu'il y a une création de base de contenu (d'où le statut = 201)
+    res.status(201).json({message: 'Objet créé !'});
 })
 
-// Ici, on va créer un moyen d'avoir une réponse à notre serveur
-// Pour voir si la requete a été envoyé ou pas
-// Ici on passe de la requete à la réponse... (voir différence par rapport au cours de Aude)
-// app.use appartient à 'Express' et sert à fixer un middleware
-// next ==> permet de renvoyer à la prochaine fonction, donc à l'autre middleware
-app.use((req, res, next) => {
-    res.json({message: 'Votre requête a bien été reçue'});
-    next();
-})
-
-// Du coup avec next, on ne peut pas modifier une réponse en cours de route
-app.use((req, res, next) => {
-    console.log("Réponse envoyée avec succès");
+// Et on fait un middelware pour 'GET'
+app.get('/api/stuff', (req, res, next) => {
+    // stuff ==> données que l'on renvoie qd quelqu'un appelle cette route
+    const stuff = [
+        {
+            // '_id' au lieu de 'id' ==> vient de mongoDB
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageURL: 'https://im.qccdn.fr/node/guide-d-achat-objectif-de-l-appareil-photo-6861/thumbnail_800x480px-120610.jpg',
+            price: 4900, // prix en centimes (on évite le moins de chiffres opossibles après la virgule)
+            userId: 'gsomihvgios'
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageURL: 'https://im.qccdn.fr/node/guide-d-achat-objectif-de-l-appareil-photo-6861/thumbnail_800x480px-120610.jpg',
+            price: 2900, // prix en centimes 
+            userId: 'gsomihvgios'
+        }
+    ];
+    
+    // On va attribuer un code '200' à la réponse
+    // Et renvoie en réponse json ==> le stuff (le tableau au-dessus avec les réponses)
+    res.status(200).json(stuff);
 })
 
 // On exporte cette fonction pour que les autres fichiers y ai accès.
